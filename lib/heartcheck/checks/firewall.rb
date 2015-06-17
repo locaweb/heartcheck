c8374debae65e8bac2f3c08a4e30b8b4664f07c9
Lucas Uyezu <lucas.uyezu@locaweb.com.br>
2015-06-17 14:59:30 -0300

    Improving error message when using proxy

--- lib/heartcheck/checks/firewall.rb
+++ lib/heartcheck/checks/firewall.rb
@@ -19,7 +19,14 @@ module Heartcheck
       private
 
       def custom_error(service)
-        @errors << "connection refused on: #{service.host}:#{service.port}"
+        error_message = "connection refused on: #{service.host}:#{service.port}"
+
+        if service.proxy
+          proxy_uri = URI(service.proxy)
+          error_message << " via proxy: #{proxy_uri.host}:#{proxy_uri.port}"
+        end
+
+        @errors << error_message
       end
     end
   end
