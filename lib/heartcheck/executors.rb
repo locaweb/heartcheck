94b05b3c6ab62737a25e39f856a83746e0ff3576
andrerocker <andre.souza@gmail.com>
2015-08-27 21:37:54 -0300

    Intruduce: executors strategy and add elapsed time
    - Now is possible to track the checker execution time
    - Now is possible to abstract to execution plan with a adapter

---
+++ lib/heartcheck/executors.rb
@@ -0,0 +1,1 @@
+require 'heartcheck/executors/base'
--- spec/lib/heart-check/controllers/essential_spec.rb
+++ spec/lib/heart-check/controllers/essential_spec.rb
@@ -6,10 +6,16 @@ module Heartcheck
       describe '#index' do
         subject(:index) { controller.index }
 
-        let(:check_01) { { dummy1: { status: :ok } } }
-        let(:check_02) { { dummy2: { status: :ok } } }
+        let(:check_01) { { dummy1: { status: :ok }, time: 1100 } }
+        let(:check_02) { { dummy2: { status: :ok }, time: 100 } }
 
         before do
+          expect(Time).to receive(:now).and_return(
+                            # millisec time calc :p
+                            0.1, 1.2, # (1.2 - 0.1) * 1000.0 = 1100
+                            2.9, 3.0  # (3.0 - 2.9) * 1000.0 = 100
+                          )
+
           Heartcheck.setup do |monitor|
             monitor.add :dummy1 do |c|
               c.add_service(name: 'dummy1')
