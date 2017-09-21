8ad5f4549b2e5343ee09f40560db39fdab6fa8a2
Estevam Souza Machado <estevam.souza@locaweb.com.br>
2017-09-21 11:13:21 -0300

    Changed tests regarding uname method to make it more independent from the implementation

--- spec/lib/heartcheck/controllers/environment_spec.rb
+++ spec/lib/heartcheck/controllers/environment_spec.rb
@@ -9,7 +9,7 @@ module Heartcheck
           stub_const('RUBY_VERSION', '2.4.0')
         end
 
-        system_info_example = { :example_key => 'example_value' }
+        let(:system_info_example) { Struct.new(:example_key).new('example_value') }
 
         context "given Rails is used" do
           before do
@@ -17,12 +17,13 @@ module Heartcheck
           end
 
           it 'gets info from the right constants and functions' do
-            allow(Sys::Uname).to receive_message_chain(:uname, :each_pair, :to_a).and_return([[:example_key, "example_value"]])
+            allow(Sys::Uname).to receive(:uname).and_return(system_info_example)
             is_expected.to include(
-              :system_info => system_info_example,
+              :system_info => { :example_key => 'example_value' },
               :ruby_version => '2.4.0',
               :rails_version => '5.0.0'
             )
+            expect(Sys::Uname).to have_received(:uname)
           end
 
           let(:system_info) do
@@ -60,12 +61,13 @@ module Heartcheck
 
         context "given Rails is not used" do
           it 'gets the info indicating Rails is not used' do
-            allow(Sys::Uname).to receive_message_chain(:uname, :each_pair, :to_a).and_return([[:example_key, "example_value"]])
+            allow(Sys::Uname).to receive(:uname).and_return(system_info_example)
             is_expected.to include(
-              :system_info => system_info_example,
+              :system_info => { :example_key => 'example_value' },
               :ruby_version => '2.4.0',
               :rails_version => '(none)'
             )
+            expect(Sys::Uname).to have_received(:uname)
           end
         end
       end
