4b0ba049e050ec767442bec476fa3632090c2c13
Ricardo Hermida Ruiz <ricardo.ruiz@locaweb.com.br>
2021-04-09 18:33:07 -0300

    Refactor formatter specs

--- spec/lib/heartcheck/formatters/base_spec.rb
+++ spec/lib/heartcheck/formatters/base_spec.rb
@@ -1,20 +1,15 @@
 require 'spec_helper'
 
 describe Heartcheck::Formatters::Base do
-  subject do
-    Heartcheck.setup do |monitor|
-    end
-    Heartcheck.formatter
-  end
   describe "#format" do
-    let(:check_01) { { dummy1: { status: :ok } , time: 1100 } }
-    let(:check_02) { { dummy2: { status: :ok } , time: 100 } }
+    let(:check_01) { {dummy1: {status: :ok}, time: 1100} }
+    let(:check_02) { {dummy2: {status: :ok}, time: 100} }
 
-    it "when hash_formatter is true" do
-      expect(subject.format([check_01,check_02])).to be_eql(
-          [{:dummy1=>{:status=>:ok}, :time=>1100},
-            {:dummy2=>{:status=>:ok}, :time=>100}])
+    it "generates a list of with a hashes for every check" do
+      expect(subject.format([check_01, check_02])).to be_eql([
+        {dummy1: {status: :ok}, time: 1100},
+        {dummy2: {status: :ok}, time: 100}
+      ])
     end
   end
-
 end
--- spec/lib/heartcheck/formatters/hash_response_spec.rb
+++ spec/lib/heartcheck/formatters/hash_response_spec.rb
@@ -1,21 +1,15 @@
 require 'spec_helper'
 
 describe Heartcheck::Formatters::HashResponse do
-  subject do
-    Heartcheck.setup do |monitor|
-      monitor.use_hash_formatter!
-    end
-    Heartcheck.formatter
-  end
   describe "#format" do
-    let(:check_01) { { dummy1: { status: :ok } , time: 1100 } }
-    let(:check_02) { { dummy2: { status: :ok } , time: 100 } }
+    let(:check_01) { {dummy1: {status: :ok}, time: 1100} }
+    let(:check_02) { {dummy2: {status: :ok}, time: 100} }
 
-    it "when hash_formatter is true" do
-      expect(subject.format([check_01,check_02])).to be_eql(
-        {:dummy1=>{:status=>:ok, :time=>1100},
-         :dummy2=>{:status=>:ok, :time=>100}})
+    it "generates a single hash with every check as a key" do
+      expect(subject.format([check_01, check_02])).to be_eql({
+        dummy1: {status: :ok, time: 1100},
+        dummy2: {status: :ok, time: 100}
+      })
     end
   end
-
 end
